d659343b0b74969c3d235f0f0d94eae02e564fac
Gustavo Todeschini <gutodeschinii@outlook.com>
2024-04-15 21:16:12 -0300

    Operadores de comparação e operador ternário

---
+++ index.js
@@ -0,0 +1,15 @@
+/* - PARTE 1
+var valor1, valor2, total;
+
+valor1 = 8;
+valor2 = "8";
+
+total = (valor1 === valor2); // true (verdadeiro) ou false (falso)
+alert(total);
+*/
+
+var idade, eleitor;
+idade = 18;
+eleitor = (idade<18) ? "Não, eleitor":"Sim, eleitor";
+
+alert('A resposta é: ' + eleitor);
